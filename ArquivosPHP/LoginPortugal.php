fece60774e9f7ad11ba91e29d0e5ca8b42ecbba8
julianoalessandro08@gmail.com <julianoalessandro08@gmail.com>
2023-11-05 21:03:41 -0300

    testando para ver se funcionar a conexao do commit

--- ArquivosPHP/LoginPortugal.php
+++ ArquivosPHP/LoginPortugal.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>LoginAdminstrador</title>
 </head>
 <body>
     
-</body>
-</html>
 <form action="usuario-login.php" method="post">
     <input class="inputs" type="text" name="nome" placeholder="Digite seu nome"><br><br>
     <input class="inputs" type="password" name="senha" placeholder="Digite sua senha"><br><br>
     <input  class="enviar2" type="submit" value="logar"><br><br>
 </form>    
+    
+</body>
