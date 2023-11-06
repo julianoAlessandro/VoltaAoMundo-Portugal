41daa1837feb17e69ede50d5e255d73db97d11d3
julianoalessandro08@gmail.com <julianoalessandro08@gmail.com>
2023-11-05 21:07:17 -0300

    testando conexao do arquivo com o repositorio

--- ArquivosPHP/LoginPortugal.php
+++ ArquivosPHP/LoginPortugal.php
@@ -12,6 +12,8 @@
     <input class="inputs" type="text" name="nome" placeholder="Digite seu nome"><br><br>
     <input class="inputs" type="password" name="senha" placeholder="Digite sua senha"><br><br>
     <input  class="enviar2" type="submit" value="logar"><br><br>
+
+    oiiiiiiiii
 </form>    
     
 </body>
