2153dd87b2c0137bfcf30ac239956dac6acd6a1a
julianoalessandro08@gmail.com <julianoalessandro08@gmail.com>
2023-11-06 17:41:43 -0300

    inicializando pagina faleConosco e Login

--- ArquivosPHP/FaleConosco.php
+++ ArquivosPHP/FaleConosco.php
@@ -5,21 +5,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="faleconosco.css">
     <title>Fale Conosco</title>
 </head>
 <body>
+    <div class="centralizar">
     <form action="CadastroGravar.php" method="post">
-        <h1>Diga para nois quais são seus lugares favoritos em Portugal</h1>
+        <fieldset>
+        <h3>O que voce mais curtiu  no site volta ao mundo?</h3>
         <label for = "nome">Nome:</label>
-        <input type="text" name = "nome"><br>
+        <input type="text" name = "nome" class="entrada"><br>
         <label for="email">Email:</label>
-        <input type="email" name="email"><br>
+        <input type="email" name="email" class="entrada"><br>
         <label for="comentario">Comentário:</label>
         <br>
         
         
-        <textarea id="comentario" name="comentario" rows="5" cols="40"></textarea><br>
-        <input type="submit" value="ENVIAR">
+        <textarea id="comentario" name="comentario"  rows="5" cols="40"></textarea><br><br>
+        <div class="central2">
+        <input type="submit" value="ENVIAR" class="botaoenviar">
+        </div>
     </form>
+</fildset>
+</div>
 </body>
 </html>
--- ArquivosPHP/LoginPortugal.php
+++ ArquivosPHP/LoginPortugal.php
@@ -4,32 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <Style>
-        .centralizar{
-            display:flex;
-            justify-content:center;
-            align-items: end;
-            padding:80px;
-            
-        
-        }
-        h1{
-            text-align:center;
-        }
-    </Style>
+    <link rel="stylesheet" href="estiloLogin.css">
     <title>LoginAdminstrador</title>
 </head>
 <body>
-<h1>Login  do Adminstrador do Sistema</h1>
+
+
 <div class="centralizar">
 
 
 <form action="usuario-login.php" method="post">
-    <input class="inputs" type="text" name="nome" placeholder="Digite seu nome"><br><br>
-    <input class="inputs" type="password" name="senha" placeholder="Digite sua senha"><br><br>
-    <input   class="btn btn-secondary btn-lg active" type="submit" value="LOGAR"><br><br>
+<fieldset>
+<h3>Login  do Adminstrador do Sistema</h3>
+<div class="centralizarformulario">
+    <input class="entrada" type="text" name="nome" placeholder="Digite seu nome"><br><br>
+    <input class="entrada" type="password" name="senha" placeholder="Digite sua senha"><br><br>
 
-    
-</form>    
 </div>
+    
+<input   type="submit" class="botaoenviar" value="LOGAR"><br><br>
+</form>
+
+</div>
+
+</fieldset>
 </body>
