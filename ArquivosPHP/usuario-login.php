6a11a0742fdf565470d14310dfae1197d27a9c37
julianoalessandro08@gmail.com <julianoalessandro08@gmail.com>
2023-11-08 00:14:58 -0300

    Sistema de Login com segurança deu certo,falta a adição do estilo

--- ArquivosPHP/usuario-login.php
+++ ArquivosPHP/usuario-login.php
@@ -3,13 +3,13 @@ require_once  'usuario-verifica.php';
 
 $nome = $_POST['nome'];
 $senhaLimpa = $_POST['senha'];
-$senha = hash("sha256",$senhaLimpa);
+$senha = hash("md5",$senhaLimpa);
 $conexao = new PDO('mysql:host=127.0.0.1;dbname=loginportugal','root','');
 $sql = "SELECT * FROM administrador WHERE nome = :user and senha = :password";
 echo $sql;
 $resultado = $conexao ->prepare($sql);
 $resultado ->bindParam(':user', $nome);
-$resultado ->bindParam(':password', $password);
+$resultado ->bindParam(':password', $senha);
 $resultado ->execute();
 $linha = $resultado ->fetch();
 $usuario_logado = $linha['nome'];
